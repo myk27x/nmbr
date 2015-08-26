b769b0bbf1f5a20516033d87e99af0e066e37b43
Michael Reed <myk27x@yahoo.com>
2015-08-26 14:30:16 -0400

    adds prompt user for 'guess'

--- guessing_game.rb
+++ guessing_game.rb
@@ -3,3 +3,19 @@
 #  Pick a random number between 1 and 100
 secret_num = rand(0...101)  # => 27
 puts secret_num # check test line **REMOVE**
+
+#  Prompt you for a guess
+puts "I'm thinking of a number from 1 to 100..."
+puts "Can you guess my number?"
+puts "What's your guess?"
+guess = gets
+puts guess # check test line **REMOVE**
+
+#  If your guess is less than the program's number, it should tell you that you were low and let you guess again.
+#  If the guess is greater than the program's number, it should tell you that you were high and let you guess again.
+#  If your guess is correct, the program should tell you that you win and then quit.
+#  After 5 incorrect guesses, the program should tell you that you lose.
+#  Your code should use at least two methods.
+#  This game should be run from the command line by typing "ruby guessing_game.rb".
+
+# >> 27
