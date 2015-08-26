3559d19d170a87dba4f8e5daecb5ae4dda4e12fa
Michael Reed <myk27x@yahoo.com>
2015-08-26 19:42:46 -0400

    lose after 5 guesses

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,13 +1,16 @@
 secret_num = rand(1..100)
 puts secret_num # check test line **REMOVE**
+guesses = 0
 
 puts "I'm thinking of a number from 1 to 100..."
-puts "Can you guess my number?"
+puts "You only get 5 tries."
 puts "What's your guess?"
 guess = gets.to_i
+guesses = guesses + 1
+puts guesses # check test line **REMOVE**
 puts guess # check test line **REMOVE**
 
-while [guess != secret_num]
+while guesses <= 5
   if guess == secret_num
     puts "YOU GOT ME!"
     exit
@@ -17,5 +20,13 @@ while [guess != secret_num]
   else
     puts "Too high... try again..."
   end
+  if guesses == 5
+    puts "NOPE! It was #{secret_num}!"
+    puts "I WIN!!! YOU LOSE!!! I WIN!!! YOU LOSE!!!"
+    exit
+  end
   guess = gets.to_i
+  guesses = guesses +1
+  puts guesses # check test line **REMOVE**
+  puts guess # check test line **REMOVE**
 end
