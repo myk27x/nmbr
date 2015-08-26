8b3bc386447df51f749933643bc25b823703c977
Michael Reed <myk27x@yahoo.com>
2015-08-26 17:57:34 -0400

    too high, try again

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,4 +1,4 @@
-secret_num = rand(1..101)
+secret_num = rand(1..100)
 puts secret_num # check test line **REMOVE**
 
 puts "I'm thinking of a number from 1 to 100..."
@@ -9,4 +9,9 @@ puts guess # check test line **REMOVE**
 
   if guess < secret_num
     puts "Too low... try again..."
+
+  else
+    puts "Too high... try again..."
+
   end
+  guess = gets.to_i
