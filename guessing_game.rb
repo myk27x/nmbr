9e8f45d05dfc5ddc6169005e457d0bb7d00e8ce6
Michael Reed <myk27x@yahoo.com>
2015-08-26 22:10:44 -0400

    sore winner/loser methods added

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,23 +1,32 @@
-secret_num = rand(1..100)
-puts secret_num # check test line **REMOVE**
+def sore_winner(message)
+  puts " _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ "
+  puts message
+  puts " -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- "
+end
+
+def sore_loser(message)
+  puts [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+  puts message
+  puts " ::DIES:: "
+end
+
 guesses = 0
+secret_num = rand(1..100)
 
 puts "I'm thinking of a number from 1 to 100..."
 puts "You only get 5 tries."
 puts "What's your guess?"
 guess = gets.to_i
 guesses = guesses + 1
-puts guesses # check test line **REMOVE**
-puts guess # check test line **REMOVE**
 
 while guesses <= 5
   if guess == secret_num
-    puts "YOU GOT ME!"
+    sore_loser("YOU GOT ME!!!")
     exit
   end
   if guesses == 5
     puts "NOPE! It was #{secret_num}!"
-    puts "I WIN!!! YOU LOSE!!! I WIN!!! YOU LOSE!!!"
+    sore_winner("||I WIN!!! YOU LOSE!!! I WIN!!! YOU LOSE!!!||")
     exit
   end
   if guess < secret_num
@@ -27,6 +36,4 @@ while guesses <= 5
   end
   guess = gets.to_i
   guesses = guesses +1
-  puts guesses # check test line **REMOVE**
-  puts guess # check test line **REMOVE**
 end
