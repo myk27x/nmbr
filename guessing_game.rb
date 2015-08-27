b61499d700d76af7bcc7f0e641a3ba011dd98c9b
Michael Reed <myk27x@yahoo.com>
2015-08-27 01:39:58 -0400

    completed adventure mode *final*

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,3 +1,13 @@
+guesses = []
+guess_count = 0
+secret_num = rand(1..100)
+
+def intro_border(message)
+  puts " _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ "
+  puts message
+  puts " -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- "
+end
+
 def sore_winner(message)
   puts " _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ "
   puts message
@@ -10,21 +20,28 @@ def sore_loser(message)
   puts " ::DIES:: "
 end
 
-guesses = 0
-secret_num = rand(1..100)
+intro_border("||I'm thinking of a number from 1 to 100...||
+||       You only get 5 tries.             ||
+||        What's your guess?               ||")
 
-puts "I'm thinking of a number from 1 to 100..."
-puts "You only get 5 tries."
-puts "What's your guess?"
 guess = gets.to_i
-guesses = guesses + 1
+guess_count = guess_count + 1
 
-while guesses <= 5
-  if guess == secret_num
-    sore_loser("YOU GOT ME!!!")
-    exit
+def duh_check(message)
+  puts message
+end
+
+while guess_count <= 5
+  guesses.each do |x|
+    if x == guess
+      duh_check("Way to waste a guess... now I'll surely win!")
+    end
   end
-  if guesses == 5
+  if guess == secret_num
+  sore_loser("YOU GOT ME!!!")
+  exit
+  end
+  if guess_count == 5
     puts "NOPE! It was #{secret_num}!"
     sore_winner("||I WIN!!! YOU LOSE!!! I WIN!!! YOU LOSE!!!||")
     exit
@@ -34,6 +51,7 @@ while guesses <= 5
   else
     puts "Too high... try again..."
   end
+  guesses << guess
   guess = gets.to_i
-  guesses = guesses +1
+  guess_count = guess_count +1
 end
